6547d1e0ff9cfbe68118b89c8cf9a1f664411992
David Hewitt (OSV) <david.hewitt.osv@fedex.com>
2022-08-18 13:11:17 -0400

    Initial commit - Trip Planner

---
+++ TripPlanner/TripPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlannerApp.swift
+//  TripPlanner
+//
+//  Created by David Hewitt on 2022-08-18.
+//
+
+import SwiftUI
+
+@main
+struct TripPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
